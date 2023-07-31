b8a2e06f3802183ad1571039e2ac5492f8e3ec44
AndrewCheUA <andreicheban@gmail.com>
2023-07-31 23:42:38 +0300

    predict page update

--- pages/1_Predict.py
+++ pages/1_Predict.py
@@ -1,25 +1,59 @@
 import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 from function.function import get_image_dimensions, is_image
+from PIL import Image
+import numpy as np
+
 
 
 st.set_page_config(page_title="Web_App_Team_3", page_icon="👨‍🎓", layout="wide")
 
+
+# Define a function to preprocess the uploaded image
+def preprocess_image(image):
+    test_image = Image.open(image)
+    target_size = (32, 32)
+    test_image = test_image.resize(target_size)
+
+    image_array = np.array(test_image)
+
+    image_array = image_array / 255.0
+
+    input_image = np.expand_dims(image_array, axis=0)
+    return input_image
+
 st.title(':red[Download picture/Завантажте картинку]', help='help')
 image = st.file_uploader("", help='help')
+
 if image:
     if is_image(image):
         st.balloons()
         st.image(image)
         width, height = get_image_dimensions(image)
-        st.write("Ширина:", width)
-        st.write("Высота:", height)
+        st.write("Width:", width)
+        st.write("Height:", height)
+
+        # Check if the user has clicked the prediction button
+        if st.button("Predict"):
+            # Load the trained model
+            model_path = 'trained_model.h5'  # Replace with the actual path to the saved model
+            loaded_model = load_model(model_path)
+
+            # Preprocess the image for prediction
+            processed_image = preprocess_image(image)
+
+            # Make prediction using the loaded model
+            predictions = loaded_model.predict(processed_image)
+
+            cifar10_classes = [
+                'airplane', 'automobile', 'bird', 'cat', 'deer',
+                'dog', 'frog', 'horse', 'ship', 'truck'
+            ]
+
+            predicted_class_index = np.argmax(predictions)
+            predicted_class_label = cifar10_classes[predicted_class_index]
+
+            st.write("Predicted Class:", predicted_class_label)
     else:
-        st.error('This file is not picture, try again', icon="🚨")
-st.title(':blue[Predict:]', help='help')
-# if image:
-#     # Путь к файлу сохраненной модели
-#     model_path = 'path/to/your/model.h5'
-#     # Загрузка модели
-#     # loaded_model = load_model(model_path)
-#     # Apply model to make predictions
-#     # prediction = loaded_model.predict(image)
+        st.error('This file is not a picture, try again', icon="🚨")
